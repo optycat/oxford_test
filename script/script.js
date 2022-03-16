d0ab4c29296f161ea99e60ca1c85bc6f608bacfe
Pulupenko Alexey <optycat@gmail.com>
2022-03-16 17:32:55 +0300

    Commit one

---
+++ script/script.js
@@ -0,0 +1,67 @@
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 1,
+  clickable: true,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 1,
+    },
+    1024: {
+      slidesPerView: 1,
+    },
+  }
+});
+var swiper = new Swiper('.mySwiper1', {
+  slidesPerView: 3,
+  spaceBetween: 45,
+  clickable: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next1",
+    prevEl: ".swiper-button-prev1",
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 1,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  }
+});
+var swiper = new Swiper('.mySwiper2', {
+  slidesPerView: 1,
+  spaceBetween: 45,
+  clickable: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next2",
+    prevEl: ".swiper-button-prev2",
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 1,
+    },
+    1024: {
+      slidesPerView: 1,
+    },
+  }
+});
